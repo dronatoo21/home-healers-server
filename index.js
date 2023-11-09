9f339081950a33e884d9258f364c666b914fbaa8
dronatoo <mrdronatoo@gmail.com>
2023-11-09 05:34:03 -0800

    added to vercel

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function run() {
       const result = await servicesCollection.updateOne(filter, service, options)
       res.send(result)
     })
-
+  // delete
     app.delete('/myServices/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)}
