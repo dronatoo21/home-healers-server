7886ecf6bd44a7575f05cdf6b55f05caecce1229
dronatoo <mrdronatoo@gmail.com>
2023-11-09 05:56:45 -0800

    addded origin

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 
