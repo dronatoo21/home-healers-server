6351aa768781bf91a33655e33cbfaf27fc9e99e1
dronatoo <mrdronatoo@gmail.com>
2023-11-09 04:10:52 -0800

    added patch method for service status

--- index.js
+++ index.js
@@ -64,7 +64,15 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/bookings/:id', async(req, res) =>{
+    app.get('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingsCollection.findOne(query)
+      res.send(result);
+    })
+
+
+    app.patch('/orders/:id', async(req, res) =>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}
       const updatedBookings = req.body;
